09f4c478e362e52a31934a62d2edc046298bc486
RedHeadEmile <emile58000@gmail.com>
2023-09-20 10:10:45 +0200

    Question 2.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
 import Image from 'next/image'
+import {PRODUCTS_CATEGORY_DATA} from "tp-kit/data";
+import {BreadCrumbs, Heading, ProductCardLayout, ProductGridLayout, SectionContainer} from "tp-kit/components";
 
 export default function Home() {
+  const categories = PRODUCTS_CATEGORY_DATA;
+
   return (
-    <main></main>
+    <main>
+      <BreadCrumbs items={ [{ label: 'Accueil', url: '/' }] } />
+      { categories.map(category => {
+        return <SectionContainer key={category.id}>
+            <Heading className={"my-4"} as={"h3"} weight={"bold"}>{ category.name + " (" + category.products.length + ")" }</Heading>
+            <ProductGridLayout products={category.products} >
+                {product => <ProductCardLayout product={product}  button={"Ajouter au panier"}/>}
+            </ProductGridLayout>
+        </SectionContainer>
+      }) }
+    </main>
   )
 }
