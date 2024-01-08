8858af3291dca784af6f3590d182c37f4f023eef
RedHeadEmile <emile58000@gmail.com>
2024-01-08 10:03:38 +0100

    Question 3.2

--- src/app/mon-compte/commandes/[orderId]/order-dialog.tsx
+++ src/app/mon-compte/commandes/[orderId]/order-dialog.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import {Fragment} from "react";
 import {Dialog} from '@headlessui/react'
 import {OrderData} from "tp-kit/types";
-import {OrderDetailsLayout} from "tp-kit/components";
+import RealTimeOrderDetail from "@/components/realtime-order-detail";
+import {useRouter} from "next/navigation";
 
 export default function OrderDialog({ order }: { order: OrderData }) {
+    const router = useRouter();
 
     return <>
-        <Dialog as="div" className="relative z-50" open={true} onClose={() => {}}>
+        <Dialog as="div" className="relative z-50" open={true} onClose={() => { console.log('closed'); router.refresh();}}>
             <Dialog.Panel className="top-0 left-0 right-0 bottom-0 fixed flex justify-center items-center">
-                <div className={"bg-white rounded p-4"}><OrderDetailsLayout order={order} /></div>
+                <div className={"bg-white rounded p-4"}><RealTimeOrderDetail order={order} /></div>
             </Dialog.Panel>
         </Dialog>
     </>
---
+++ src/components/realtime-order-detail.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import {useEffect, useState} from "react";
+import {createClientComponentClient} from "@supabase/auth-helpers-nextjs";
+import {OrderData} from "tp-kit/types";
+import {OrderDetailsLayout} from "tp-kit/components";
+
+export default function RealTimeOrderDetail({ order }: { order: OrderData }) {
+  const [realTimeOrder, setRealTimeOrder] = useState(order);
+
+  const supabase = createClientComponentClient();
+  useEffect(() => {
+    const channel = supabase
+        .channel('schema-db-changes')
+        .on(
+            'postgres_changes',
+            {
+              event: 'UPDATE',
+              schema: 'public',
+              table: 'Order',
+              filter: 'id=eq.' + order.id
+            },
+            (payload) => {
+              const newValues = payload.new;
+              setRealTimeOrder(old => { return {...old, ...newValues}; });
+            }
+        )
+        .subscribe();
+
+    return () => { channel.unsubscribe().then(r => {}); };
+  }, [supabase]);
+
+  return <OrderDetailsLayout order={realTimeOrder} />;
+}
